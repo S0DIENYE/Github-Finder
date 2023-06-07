1058e20ba1dbcb436b4c06790b0b3cae1ad4b511
S0DIENYE <peppledavid29@gmail.com>
2023-06-07 08:25:10 +0100

    progress

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -6,10 +6,12 @@ const User = (props) => {
   const routeParams = useParams();
   
   useEffect(() => {
-    // this.props.getUser(this.props.match.params.login)
+    // props.getUser(props.match.params.login)
     // console.log(routeParams);
+    props.getUser(routeParams.login)
+    console.log(props.user);
     // props.getUser(routeParams.login)
-  })
+  }, [])
   /*
     Sample of useEffect
     const [properties, setPropertiesMapFunc] = useState(new Map());
@@ -19,6 +21,18 @@ useEffect(()=>
     setPropertiesMapFunc(mapNum === 1 ?propertiesMap1 : propertiesMap2);
 }, [properties]);
   */
+ const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    followers,
+    following,
+    public_repos,
+    public_gist
+ } = props.user
 
 
   return (
